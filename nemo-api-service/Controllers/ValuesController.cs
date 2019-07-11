84118ee91e75b96887694095ebc281ced4c375e0
jackyzhou <jackyzhou@leansoftx.com>
2019-07-11 10:22:22 +0800

    add Kubernetes Client SDK

--- nemo-api-service/Controllers/ValuesController.cs
+++ nemo-api-service/Controllers/ValuesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using k8s;
 
 namespace nemo_api_service.Controllers
 {
@@ -14,6 +15,20 @@ namespace nemo_api_service.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
+            var config = KubernetesClientConfiguration.BuildConfigFromConfigFile();
+            // Use the config object to create a client.
+            var client = new Kubernetes(config);
+            var namespaces = client.ListNamespace();
+            foreach (var ns in namespaces.Items)
+            {
+                Console.WriteLine(ns.Metadata.Name);
+                var list = client.ListNamespacedPod(ns.Metadata.Name);
+                foreach (var item in list.Items)
+                {
+                    Console.WriteLine(item.Metadata.Name);
+                }
+            }
+
             return new string[] { "value1", "value2" };
         }
 
