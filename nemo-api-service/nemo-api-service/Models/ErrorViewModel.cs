449751063928eb59fa0859752806fe98fa6979c2
jackyzhou <jackyzhou@leansoftx.com>
2019-07-11 09:54:54 +0800

    init

---
+++ nemo-api-service/nemo-api-service/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace nemo_api_service.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
