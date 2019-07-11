638961a7c3ae067f52220928088bdd29bda419e6
Administrator <Administrator@WIN-L2RC0MT8IE5>
2019-07-10 20:36:53 -0700

    add namespace list

---
+++ nemo-api-service/Controllers/NamespacesController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using k8s;
+
+namespace nemo_api_service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NamespacesController : ControllerBase
+    {
+        
+
+        // GET: api/Namespaces
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var client = library.Base.getClient();
+            var namespaces = client.ListNamespace();
+            return Ok(namespaces.Items);
+        }
+
+        // GET: api/Namespaces/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Namespaces
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Namespaces/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ nemo-api-service/library/Base.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using k8s;
+namespace nemo_api_service.library
+{
+    public static class Base {
+        public static IKubernetes getClient()
+        {
+            var config = KubernetesClientConfiguration.BuildConfigFromConfigFile();
+            var client = new Kubernetes(config);
+            return client;
+        }
+    }
+
+}
