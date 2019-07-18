acb0026af3371074e2925cce2c233e8a098d80ee
Administrator <Administrator@WIN-L2RC0MT8IE5>
2019-07-18 01:18:03 -0700

    change to stream way

---
+++ nemo-api-service/library/Utilities.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace nemo_api_service.library
+{
+    public static class Utilities
+    {
+        public static Stream GenerateStreamFromString(string s)
+        {
+            var stream = new MemoryStream();
+            var writer = new StreamWriter(stream);
+            writer.Write(s);
+            writer.Flush();
+            stream.Position = 0;
+            return stream;
+        }
+    }
+}
